58bade64fccefe221d6ada9fa7a56b042f5fecf5
innTall <avg999.trade@gmail.com>
2025-01-10 14:52:17 +0100

    0.0.1 Margin Settings

---
+++ src/stores/marginSettings.js
@@ -0,0 +1,60 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useMarginSettingsStore = defineStore(
+  "marginSettings",
+  () => {
+    const openSettings = ref(false);
+    const deposit = ref(null);
+    const leverage = ref(null);
+    const coefRisk = ref(null);
+    const coefTP = ref(null);
+		const coefSL = ref(null);
+		const coefOrder = ref(null);
+		const feeBuy = ref(0.02);
+		const feeSell = ref(0.055);
+
+    // Actions
+    const toggleSettings = () => {
+      openSettings.value = !openSettings.value;
+    };
+
+    const setSettings = (newSettings) => {
+      if (newSettings.deposit !== undefined) deposit.value = newSettings.deposit;
+      if (newSettings.leverage !== undefined) leverage.value = newSettings.leverage;
+      if (newSettings.coefRisk !== undefined) coefRisk.value = newSettings.coefRisk;
+      if (newSettings.coefTP !== undefined) coefTP.value = newSettings.coefTP;
+      if (newSettings.coefSL !== undefined)	coefSL.value = newSettings.coefSL;
+			if (newSettings.coefOrder !== undefined) coefOrder.value = newSettings.coefOrder;
+      if (newSettings.feeBuy !== undefined) feeBuy.value = newSettings.feeBuy;
+      if (newSettings.feeSell !== undefined) feeSell.value = newSettings.feeSell;
+    };
+
+    const resetSettings = () => {
+      deposit.value = 0;
+      leverage.value = 0;
+      coefRisk.value = 0;
+      coefTP.value = 0;
+			coefSL.value = 0;
+			coefOrder = 0;
+      feeBuy = 0.02;
+      feeSell = 0.055;
+    };
+
+    return {
+      openSettings,
+      deposit,
+      leverage,
+      coefRisk,
+      coefTP,
+			coefSL,
+			coefOrder,
+      feeBuy,
+      feeSell,
+      toggleSettings,
+      setSettings,
+      resetSettings,
+    };
+  },
+  { persist: true }
+);

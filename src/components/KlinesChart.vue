b5bdd055fc20269e913152d2834048b3e6a85580
Leon2ev <jevgeni.glazunov@gmail.com>
2025-01-21 20:54:08 +0100

    Fix realtime chart

--- src/components/KlinesChart.vue
+++ src/components/KlinesChart.vue
@@ -1,12 +1,32 @@
 <script setup>
-import { storeToRefs } from "pinia";
-import { onMounted, watch } from "vue";
+import { onMounted, onBeforeUnmount } from "vue";
 import { createChart } from "lightweight-charts";
-import { useKlinesChartStore } from "@/stores/klinesChart.js";
-const { klineData } = storeToRefs(useKlinesChartStore());
-const { initializeChart } = useKlinesChartStore();
+import getKlines from "@/api/bybit/getKlines.js";
+
+const wsUrl = "wss://stream.bybit.com/v5/public/linear";
+const interval = '15';
+const symbol = 'BTCUSDT';
+
+let ws;
 let chart;
 let candlestickSeries;
+
+const parseKlines = ({ result }) => {
+	if (result && result.list) {
+		return result.list
+			.map((kline) => ({
+				time: Math.floor(kline[0] / 1000),
+				open: parseFloat(kline[1]),
+				high: parseFloat(kline[2]),
+				low: parseFloat(kline[3]),
+				close: parseFloat(kline[4]),
+			}))
+			.sort((a, b) => a.time - b.time);
+	}
+
+	return [];
+}
+
 onMounted(() => {
 	chart = createChart(document.getElementById("chart"), {
 		width: document.getElementById("chart").clientWidth,
@@ -24,21 +44,69 @@ onMounted(() => {
 			},
 		},
 	});
+
 	candlestickSeries = chart.addCandlestickSeries();
-	initializeChart();
-	let timeout;
-	watch(
-		() => klineData.value,
-		(newData) => {
-			clearTimeout(timeout);
-			timeout = setTimeout(() => {
-				candlestickSeries.setData(newData);
-			}, 100); // Debounce updates
-		},
-		{ deep: true }
-	);
+})
+
+onMounted(async () => {
+	const klines = await getKlines(symbol, interval);
+	candlestickSeries.setData(parseKlines(klines));
+});
+
+onMounted(() => {
+	ws = new WebSocket(wsUrl);
+
+	ws.onopen = () => {
+		console.log("WebSocket connection opened.");
+		const payload = {
+			op: "subscribe",
+			args: [`kline.${interval}.${symbol}`],
+		};
+		ws.send(JSON.stringify(payload));
+	};
+
+	ws.onmessage = (event) => {
+		try {
+			const message = JSON.parse(event.data);
+			if (
+				message &&
+				message.topic === `kline.${interval}.${symbol}` &&
+				Array.isArray(message.data) &&
+				message.data.length > 0
+			) {
+				const rawData = message.data[0];
+				const formattedData = {
+					time: Math.floor(rawData.start / 1000),
+					open: parseFloat(rawData.open),
+					high: parseFloat(rawData.high),
+					low: parseFloat(rawData.low),
+					close: parseFloat(rawData.close),
+				};
+				candlestickSeries.update(formattedData);
+			}
+		} catch (error) {
+			console.error("Failed to process WebSocket message:", error);
+		}
+	};
+
+	ws.onclose = () => {
+		console.log("WebSocket connection closed.");
+	};
+
+	ws.onerror = (error) => {
+		console.error("WebSocket error:", error);
+	};
+})
+
+onBeforeUnmount(() => {
+	if (ws) {
+		ws.close();
+		ws.value = null;
+		console.log('WebSocket disconnected');
+	}
 });
 </script>
+
 <template>
 	<div class="flex items-center justify-center">
 		<div id="chart" class="w-full h-96"></div>
--- src/views/MarginTrade.vue
+++ src/views/MarginTrade.vue
@@ -1,15 +1,14 @@
 <script setup>
 import MarginOptions from '../components/MarginOptions.vue';
 import OrdersBlock from '../components/OrdersBlock.vue';
-//import KlinesChart from '../components/KlinesChart.vue';
-import KlinesData from '../components/KlinesData.vue';
+import KlinesChart from '../components/KlinesChart.vue';
 </script>
 
 <template>
 	<div class="flex flex-col h-full">
 		<MarginOptions />
 		<OrdersBlock />
-		<KlinesData />
+		<KlinesChart />
 	</div>
 </template>
 <style scoped></style>

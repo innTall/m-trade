1ecde2916f0293f7fa85ff4b7692ec1c478b178f
innTall <avg999.trade@gmail.com>
2025-01-21 12:53:08 +0100

    0.0.5 KlinesData from API & WebSocket

--- src/utils/webSocketBybit.js
+++ src/utils/webSocketBybit.js
@@ -1,23 +1,19 @@
-export function setupWebSocket(
-  baseAsset,
-  quoteAsset,
-  interval,
-  onMessageCallback
-) {
-  const symbol = `${baseAsset.toUpperCase()}${quoteAsset.toUpperCase()}`;
+export function initializeWebSocket(symbol, interval, onMessageCallback) {
   const wsUrl = "wss://stream.bybit.com/v5/public/linear";
   let ws = null;
+
   const connect = () => {
     ws = new WebSocket(wsUrl);
+
     ws.onopen = () => {
-      console.log("WebSocket connected to:", wsUrl);
-      ws.send(
-        JSON.stringify({
-          op: "subscribe",
-          args: [`kline.${interval}.${symbol}`],
-        })
-      );
+      console.log("WebSocket connection opened.");
+      const payload = {
+        op: "subscribe",
+        args: [`kline.${interval}.${symbol}`],
+      };
+      ws.send(JSON.stringify(payload));
     };
+
     ws.onmessage = (event) => {
       try {
         const message = JSON.parse(event.data);
@@ -27,18 +23,27 @@ export function setupWebSocket(
           Array.isArray(message.data) &&
           message.data.length > 0
         ) {
-          onMessageCallback(message.data);
+          const rawData = message.data[0];
+          const formattedData = {
+            time: Math.floor(rawData.start / 1000),
+            open: parseFloat(rawData.open),
+            high: parseFloat(rawData.high),
+            low: parseFloat(rawData.low),
+            close: parseFloat(rawData.close),
+          };
+          onMessageCallback(formattedData);
         }
       } catch (error) {
         console.error("Failed to process WebSocket message:", error);
       }
     };
+
+    ws.onclose = () => {
+      console.log("WebSocket connection closed.");
+    };
+
     ws.onerror = (error) => {
       console.error("WebSocket error:", error);
-    };
-    ws.onclose = (e) => {
-      console.warn("WebSocket closed. Reconnecting in 5 seconds...", e);
-      setTimeout(connect, 5000); // Retry connection after 5 seconds
     };
   };
   connect();
--- src/views/MarginTrade.vue
+++ src/views/MarginTrade.vue
@@ -1,14 +1,15 @@
 <script setup>
 import MarginOptions from '../components/MarginOptions.vue';
 import OrdersBlock from '../components/OrdersBlock.vue';
-import KlinesChart from '../components/KlinesChart.vue';
+//import KlinesChart from '../components/KlinesChart.vue';
+import KlinesData from '../components/KlinesData.vue';
 </script>
 
 <template>
 	<div class="flex flex-col h-full">
 		<MarginOptions />
 		<OrdersBlock />
-		<KlinesChart />
+		<KlinesData />
 	</div>
 </template>
 <style scoped></style>

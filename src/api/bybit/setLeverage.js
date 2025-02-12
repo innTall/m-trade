5e87153c3f95f3175064967986d36a80f29bce19
Leon2ev <jevgeni.glazunov@gmail.com>
2025-02-12 14:16:18 +0100

    add set levergae api

---
+++ src/api/bybit/setLeverage.js
@@ -0,0 +1,17 @@
+import fetchWrapper from './fetchWrapper';
+// https://bybit-exchange.github.io/docs/v5/position/leverage
+export default async function setLeverage({ symbol, leverage }) {
+  const path = '/position/leverage';
+  const data = await fetchWrapper({
+    path,
+    isPrivate: true,
+    method: 'POST',
+    body: JSON.stringify({
+      category: 'linear',
+      symbol,
+      buyLeverage: leverage,
+      sellLeverage: leverage,
+    }),
+  });
+  return data?.retMsg === 'OK';
+}

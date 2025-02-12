65450daab845eabf89e4584a76cac309719bc623
Leon2ev <jevgeni.glazunov@gmail.com>
2025-02-12 13:50:54 +0100

    Delete spot margin store

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,14 @@ import { storeToRefs } from 'pinia';
 import HeaderBar from './components/HeaderBar.vue';
 import { useInstrumentInfoStore } from './stores/instrumentInfoStore';
 import { useAccountStore } from './stores/accountStore';
-import { useSpotMarginStore } from './stores/spotMarginStore';
 
 // Load initial data
 const instrumentInfoStore = useInstrumentInfoStore();
 const accountStore = useAccountStore();
-const spotMarginStore = useSpotMarginStore();
 const { fetchInstrumentInfo } = instrumentInfoStore;
 const { fetchAccount } = accountStore;
-const { fetchSpotMarginState } = spotMarginStore;
 const { instrumentInfo } = storeToRefs(instrumentInfoStore);
 const { account } = storeToRefs(accountStore);
-const { state } = storeToRefs(spotMarginStore);
 
 onMounted(async () => {
   if (!instrumentInfo.length) {
@@ -24,10 +20,6 @@ onMounted(async () => {
 
   if (!account.length) {
     await fetchAccount();
-  }
-
-  if (!state.value) {
-    await fetchSpotMarginState();
   }
 });
 </script>

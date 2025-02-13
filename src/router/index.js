a94320a609750034b096c7b0cb794ed2dfae78fc
Leon2ev <jevgeni.glazunov@gmail.com>
2025-02-13 10:49:00 +0100

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import MarginTrade from '../views/MarginTrade.vue';
+import TradingPage from '../views/TradingPage.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'MarginTrade',
-      component: MarginTrade,
+      path: '/trading',
+      name: 'trading',
+      component: TradingPage,
     },
   ],
 });
--- src/views/TradingPage.vue
+++ src/views/TradingPage.vue
@@ -1,21 +1,11 @@
 <script setup>
-import MarginOptions from '@/components/MarginOptions.vue';
 import KlinesChart from '@/components/KlinesChart.vue';
-import MarginSettings from '@/components/MarginSettings.vue';
 import OrderList from '@/components/OrderList.vue';
 import OrderPlacement from '@/components/OrderPlacement.vue';
 </script>
 
 <template>
   <div class="h-full overflow-y-scroll">
-    <div class="flex p-2 bg-surface-800 gap-2">
-      <div class="flex-grow">
-        <MarginOptions />
-      </div>
-      <div class="flex-shrink">
-        <MarginSettings />
-      </div>
-    </div>
     <KlinesChart />
     <OrderList />
     <OrderPlacement />

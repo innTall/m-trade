7f3f855a5011639ed4423453f61e42e5163b1cef
Leon2ev <jevgeni.glazunov@gmail.com>
2025-02-21 16:08:13 +0100

    toast on top

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Toast from 'primevue/toast';
 </script>
 
 <template>
-  <Toast position="bottom-center" class="!px-8" />
+  <Toast position="top-center" class="!px-8" />
   <RouterView />
 </template>
 <style scoped></style>

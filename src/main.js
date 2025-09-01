b033a169af55ecaf0d84222a4c2a6291f364ac04
innTall <avg999.trade@gmail.com>
2025-09-01 07:03:43 +0200

    small commit

--- src/main.js
+++ src/main.js
@@ -1,31 +1,13 @@
 import {createApp} from "vue";
 import {createPinia} from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import PrimeVue from "primevue/config";
-import Aura from "@primevue/themes/aura";
-import ToastService from "primevue/toastservice";
 
 import App from "./App.vue";
 import router from "./router/index.js";
-
-import "primeicons/primeicons.css";
 import "./style.css";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 pinia.use(piniaPluginPersistedstate);
-
-app
-  .use(PrimeVue, {
-    theme: {
-      preset: Aura,
-      options: {
-        darkModeSelector: ".app-dark"
-      }
-    }
-  })
-  .use(ToastService)
-  .use(pinia)
-  .use(router)
-  .mount("#app");
+app.use(pinia).use(router).mount("#app");
